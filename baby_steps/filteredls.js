c72d201b4344c81e06912ff57e2f762eea945acd
ekolker <e.kolker@gmail.com>
2014-02-24 16:31:22 -0500

    maybe i get it more now?

--- baby_steps/filteredls.js
+++ baby_steps/filteredls.js
@@ -3,12 +3,15 @@ var path = require('path');
 
 // console.log(process.argv[3]);
 
-fs.readdir(process.argv[2], function (err, data)
+var directory = process.argv[2];
+var extension = process.argv[3];
+
+fs.readdir(directory, function (err, data)
 {
   for (f in data)
   {
     // console.log(f, path.extname(data[f]))
-    if (path.extname(data[f]) == "." + process.argv[3])
+    if (path.extname(data[f]) == "." + extension)
     {
       console.log(data[f]);
     }
