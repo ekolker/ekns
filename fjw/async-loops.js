ad783d375280b120514db41a62a3f74b34724e77
ekolker <e.kolker@gmail.com>
2014-02-27 13:17:43 -0500

    rtm...

--- fjw/async-loops.js
+++ fjw/async-loops.js
@@ -2,12 +2,31 @@ function loadUsers(userIds, load, done)
 {
   var users = [];
   
-  for (var i = 0; i < userIds.length; i++)
-  {
-    users.push(load(userIds[i]));
-  }
+  // for (var i = 0; i < userIds.length; i++)
+  // {
+  //   users.push(load(userIds[i]));
+  // }
 
-  return users;
+  // userIds.forEach(function(id)
+  // {
+  //   users.push(load(id, done));
+  // });
+
+
+
+  var completed = 0;
+
+  userIds.forEach(function(id, index)       //  TIL: the second argument to the callback given to forEach is the index of the element (#1) in the original array (#3)
+  {
+    load(id, function(user)
+    {
+      users[index] = user;                  //  assign it
+      if (++completed === userIds.length)   //  check it
+      {
+        return done(users);                 //  only return when we're actually done
+      }
+    });
+  });
 }
 
 module.exports = loadUsers;
